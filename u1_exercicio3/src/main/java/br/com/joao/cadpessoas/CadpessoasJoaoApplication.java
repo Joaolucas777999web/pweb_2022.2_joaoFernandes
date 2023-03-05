294a2b067c5d27a994c3a30a37dc3d181e5a0241
Joaolucas777666 <124449553+Joaolucas777999web@users.noreply.github.com>
2023-03-05 00:13:35 -0300

    exercicio 3 inicio

---
+++ u1_exercicio3/src/main/java/br/com/joao/cadpessoas/CadpessoasJoaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.joao.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasJoaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasJoaoApplication.class, args);
+	}
+
+}
---
+++ u1_exercicio3/src/test/java/br/com/joao/cadpessoas/CadpessoasJoaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joao.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasJoaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
