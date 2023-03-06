7c2d861dc2bf171e027496a990659af0a7125665
Joaolucas777666 <124449553+Joaolucas777999web@users.noreply.github.com>
2023-03-05 22:57:43 -0300

    continua sem funcionar

--- u1_exercicio3/src/main/java/br/com/joao/cadpessoas/controllers/PessoaController.java
+++ u1_exercicio3/src/main/java/br/com/joao/cadpessoas/controllers/PessoaController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,8 +29,9 @@ public class PessoaController {
 
     @GetMapping("/listarPessoas")
     public ModelAndView listarPessoas(){
-        List<Pessoa> todasAsPessoas = pessoaRepo.findAll()
+        List<Pessoa> todasAsPessoas = pessoaRepo.findAll();
         ModelAndView modelAndView = new ModelAndView("ListarPessoas");
         modelAndView.addObject("todasAsPessoas", todasAsPessoas);
         return modelAndView;
+}
 }
